23364c7ffbc5aab8bcd1acec2592886e243d6c30
Jaime Broñozo <bronozoj@gmail.com>
2018-11-19 00:55:19 +0800

    actual beta adc lib added

---
+++ utilities/adcread.c
@@ -0,0 +1,64 @@
+/**
+ * @file  adcread.c
+ * @brief This file contains function wrappers for dynamic ADC usage.
+ * @author Jaime Bronozo
+ * 
+ * This is a library for accessing the functionality of the ADC module in
+ * a manner that does not require external variables or interrupts. It also
+ * abstracts most of the work needed to set it up and makes it operate
+ * similar to the **analogRead()** function available in the Arduino
+ * framework. This library works in conjunction with the settings found in
+ * the configuration file toolbox_settings.h in order to set the proper
+ * flags to the module.
+ * 
+ * @date November 19, 2018
+ **************************************************************************/
+
+/// @cond
+#define __LIBADCREAD_SETTINGS
+
+#include "toolbox_settings.h"
+#include "adcread.h"
+
+#if __LIBADCREAD_DISABLE != 1
+/// @endcond
+
+
+/**
+ * @brief Sets up the nexessary values to read analog values.
+ * 
+ * Sets up the necessary ADC configuration flags to allow it to be used
+ * as an on-demand analog value reader. This requires the user to configure
+ * the AD1PCFG register to manually exclude pins to be used only for
+ * digital I/O purposes.
+ **************************************************************************/
+void ADC_begin(){
+    AD1CON1 = 0xE0; // set form to integer
+    AD1CON2 = 0;
+    AD1CON3bits.SAMC = _SAMPLE_PERIOD; // Tsamp
+    AD1CON3bits.ADCS = _ADC_PERIOD; // Tad
+    AD1CHS = 0;
+    AD1CONbits.ADON = 1;
+}
+
+/**
+ * @param pin Analog pin number.
+ * 
+ * @brief Reads the specified analog pin.
+ * 
+ * This returns a value proportional to the voltage drop from the pin to
+ * ground. This assumes that the pin is set up for usage as an analog
+ * reading pin (by setting the ADC pin configuration and digital pin mode).
+ * 
+ * @return A value from 0-1023 proportional to Vdd and ground.
+ **************************************************************************/
+
+uint16_t analogRead(unsigned short pin){
+    AD1CHSbits.CH0SA = pin;
+    AD1CON1bits.SAMP = 1;
+    while(!AD1CON1bits.DONE);
+    AD1CON1bits.DONE = 0;
+    return ADC1BUF0;
+}
+
+#endif
---
+++ utilities/adcread.h
@@ -0,0 +1,19 @@
+/** 
+ * @file  keypad_4x3.h
+ * @brief This file contains function wrappers for dynamic ADC usage
+ * @author Jaime Bronozo
+ * 
+ * This is a header file for adcread.c which must be included to any source
+ * files that require usage of analog reading related functions. This
+ * library is dynamically included in the main header PIC24_toolbox.h
+ * 
+ * @date November 8, 2018
+ **************************************************************************/
+
+#ifndef __ADCREAD_TOOLBOX_H__
+#define __ADCREAD_TOOLBOX_H__
+
+void ADC_begin();
+uint16_t analogRead(unsigned short pin);
+
+#endif
